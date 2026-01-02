91ec4ac98213a861360308b4ee1772fc91a9f761
davidkankowski <davidkanko12@gmail.com>
2026-01-01 18:41:30 -0800

    feat: implement ETL transformation layer and standardize data types

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
+import pandas as pd
+from datetime import datetime
 
 load_dotenv()
 
@@ -27,7 +29,7 @@ def get_pages(num_pages=None):
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code != 200:
-        print("Error before loop:", response.status_code)
+        print("Error:", response.status_code)
         print(response.text)
         return []
 
@@ -36,27 +38,72 @@ def get_pages(num_pages=None):
 
     # Loop for pagination
     while data["has_more"] and get_all:
-        print("Looping to get all pages")
         payload = {
             "page_size": page_size, 
             "start_cursor": data["next_cursor"]
         }
         response = requests.post(url, json=payload, headers=headers)
-        
-        if response.status_code != 200:
-            print("Error in loop:", response.status_code)
-            break
-            
         data = response.json()
         results.extend(data["results"])
 
-    print(f"Successfully fetched {len(results)} total rows.")
     return results
 
-if __name__ == "__main__":
-    pages = get_pages()
+def clean_notion_data(data):
+    clean_rows = []
+    
+    for page in data:
+        page_id = page['id'] 
+        props = page['properties']
+        
+        try:
+            # Habit column            
+            if props['Habit']['title']:
+                habit_name = props['Habit']['title'][0]['text']['content']
+            else:
+                habit_name = "Untitled"
 
-    # To save to a JSON file locally
-    # with open('my_data.json', 'w', encoding='utf8') as f:
-    #     json.dump(pages, f, ensure_ascii=False, indent=4)
-    #     print("Saved raw data to my_data.json")
+            # Date column
+            raw_date = props['Date']['date']['start']
+
+            # Status column
+            status_name = props['Status']['status']['name']
+
+            # Convert Status to boolean value
+            is_completed = (status_name == "Done")
+            
+            clean_rows.append({
+                            "page_id": page_id,
+                            "habit_name": habit_name,
+                            "date": raw_date,
+                            "status": status_name,       
+                            "is_completed": is_completed 
+            })
+
+        except KeyError as e:
+            print(f"Row skipped. Missing column {e}. Check Notion column names.")
+            continue
+        except TypeError as e:
+            print(f"Row skipped. Data format issue: {e}")
+            continue
+
+    df = pd.DataFrame(clean_rows)
+    
+    if not df.empty:
+        df['date'] = pd.to_datetime(df['date'])
+    
+    return df
+
+if __name__ == "__main__":
+    # Extract
+    raw_pages = get_pages()
+    print(f"Extracted {len(raw_pages)} rows.")
+    
+    # Transform
+    df = clean_notion_data(raw_pages)
+    print(f"Transformed data.")
+    
+    if not df.empty:
+        print("\n Clean Data Preview:")
+        print(df.head())
+    else:
+        print("No data found or all rows failed transformation.")
